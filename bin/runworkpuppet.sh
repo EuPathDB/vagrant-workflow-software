cae72423984ab480aa1d0c712f5ecd5db0fd5dad
Mark Heiges <mheiges@uga.edu>
2018-01-09 17:39:40 -0500

    add logging arg to runworkpuppet.sh

--- bin/runworkpuppet.sh
+++ bin/runworkpuppet.sh
@@ -2,6 +2,7 @@
 set -x
 set -e
 wfs_dir=$1
+workpuppet_log=$2
 bashrc=${wfs_dir}/sysadmin/bashrc
 source $bashrc || { echo $bashrc not found; exit 1; }
-workpuppet 2>&1 > /home/vagrant/workpuppet.log
+workpuppet 2>&1 > $workpuppet_log
